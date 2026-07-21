5ddc478c786992ed4fbac26a67e52619ced8f549
Moro2505 <95554980+Moro2505@users.noreply.github.com>
2026-07-21 21:47:55 +0300

    feat: implement upcoming renewals route and name search filter

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import Subscription from '../models/subscription.model.js';
 import { workflowClient } from '../config/upstash.js';
 import { SERVER_URL } from '../config/env.js';
@@ -32,16 +33,51 @@ workflowRunId: workflowRunId
 
 export const getSubscriptions = async (req, res, next) => {
 try {
-    const query = { user: req.user._id };
+    const { status, search } = req.query;
 
-    if (req.query.status) {
-        query.status = req.query.status;
+    const queryFilter = { user: req.user._id };
+
+    if (status) {
+        queryFilter.status = status;
     }
 
-    const subscriptions = await Subscription.find(query);
+    if (search) {
+        queryFilter.name = { $regex: search, $options: 'i' };
+    }
 
-    res.status(200).json({ success: true, count: subscriptions.length , data: subscriptions });
-} catch (e) {
-    next(e);
-}
+    const subscriptions = await Subscription.find(queryFilter).sort({ createdAt: -1 });
+
+    res.status(200).json({
+        success: true,
+        count: subscriptions.length,
+        data: subscriptions,
+    });
+    } catch (error) {
+    next(error);
+    }
+};
+
+
+export const getUpcomingRenewals = async (req, res, next) => {
+try {
+    const startDate = dayjs().toDate();
+    const endDate = dayjs().add(7, 'day').toDate();
+
+    const upcomingSubscriptions = await Subscription.find({
+        user: req.user._id,
+        status: 'active',
+        renewalDate: {
+        $gte: startDate,
+        $lte: endDate,
+        },
+    }).sort({ renewalDate: 1 });
+
+    res.status(200).json({
+        success: true,
+        count: upcomingSubscriptions.length,
+        data: upcomingSubscriptions,
+    });
+    } catch (error) {
+    next(error);
+    }
 };
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -2,14 +2,13 @@ import { Router } from 'express';
 
 import authorize from '../middlewares/auth.middleware.js';
 
-import { createSubscription , getSubscriptions } from '../controllers/subscription.controller.js';
+import { createSubscription , getSubscriptions , getUpcomingRenewals } from '../controllers/subscription.controller.js';
 
 const subscriptionRouter = Router();
 
 subscriptionRouter.post('/', authorize , createSubscription );
 subscriptionRouter.get('/', authorize , getSubscriptions);
-subscriptionRouter.get('/upcoming-renewals', (req, res) => res.send({ title: 'GET upcoming renewals' }));
-subscriptionRouter.get('/:id', (req, res) => res.send({ title: 'GET subscription details' }));
+subscriptionRouter.get('/upcoming-renewals', authorize , getUpcomingRenewals);
 subscriptionRouter.put('/:id', (req, res) => res.send({ title: 'UPDATE subscription' }));
 subscriptionRouter.put('/:id/cancel', (req, res) => res.send({ title: 'CANCEL subscription' }));
 subscriptionRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE subscription' }));
